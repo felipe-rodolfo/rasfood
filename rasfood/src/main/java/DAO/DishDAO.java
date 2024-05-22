86e2ffc14d51686b346a4c883568c655d30a2e04
Felipe Rodolfo <felipewebdeveloper@gmail.com>
2024-05-22 19:19:12 -0300

    implementing CRUD in the DAO class

--- rasfood/src/main/java/DAO/DishDAO.java
+++ rasfood/src/main/java/DAO/DishDAO.java
@@ -8,12 +8,24 @@ public class DishDAO {
 
     private EntityManager entityManager;
 
-    public DishDAO(EntityManager entityManager){
+    public DishDAO(EntityManager entityManager) {
         this.entityManager = entityManager;
     }
 
-    public void register(Dish dish){
+    public void register(final Dish dish) {
         this.entityManager.persist(dish);
         System.out.println("Entidade cadastrada: " + dish);
+    }
+
+    public Dish consult(final Integer id) {
+        return this.entityManager.find(Dish.class, id);
+    }
+
+    public void update(final Dish dish){
+        this.entityManager.merge(dish);
+    }
+
+    public void delete(final Dish dish){
+        this.entityManager.remove(dish);
     }
 }
